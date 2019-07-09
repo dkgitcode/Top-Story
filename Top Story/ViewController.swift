0e8fdb4c87ac080a27e5b2b904c4e5d0448011d1
Daniel Kim <danjkim11@gmail.com>
2019-07-09 10:07:00 -0500

    Initial Commit

---
+++ Top Story/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Top Story
+//
+//  Created by Daniel Kim on 7/9/19.
+//  Copyright © 2019 Daniel Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
